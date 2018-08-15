5be8f9c66ee250be74b5efccca6fcc10d07a6f8c
Marcin Slowikowski <sonky108@gmail.com>
2018-08-15 16:07:24 +0200

    Initial commit

---
+++ Refactor/Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	public GameObject _image;
+	public Sprite[] sprites;
+
+	public static GameManager Instance;
+
+	public int currentSprite = 0;
+
+	private void Awake()
+	{
+		Instance = this;
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			currentSprite++;
+
+			if (currentSprite >= 4)
+				currentSprite = 0;
+
+			_image.GetComponent<Image>().sprite = sprites[currentSprite];
+
+			if (_image.GetComponent<Image>().sprite.name == "mouth_lol")
+			{
+				GetComponent<AudioSource>().Play();
+			}
+
+		}
+	}
+}
