01eec998daef0434f90af0a73a3a2bfe89ec14c7
Marcin Slowikowski <sonky108@gmail.com>
2018-08-15 17:56:05 +0200

    Add man properties

---
+++ Refactor/Assets/Scripts/ManProperties.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ManProperties", menuName = "Man/Properties", order = 1)]
+public class ManProperties : ScriptableObject {
+
+	public Texture Mouth;
+	public AudioClip OnChange;
+}
