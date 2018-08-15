b49ca49fc90e1fce7eeee8d8d539ac98632cb4f6
Marcin Slowikowski <sonky108@gmail.com>
2018-08-15 16:23:46 +0200

    Add MonoBehaviourSingleton

---
+++ Refactor/Assets/Scripts/MonoBehaviourSingleton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonoBehaviourSingleton<T> : MonoBehaviour  where T : class {
+
+    public static T Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this as T;
+            DontDestroyOnLoad(this);
+        }
+        else if(Instance != this as T)
+        {
+            Debug.LogWarning($"Destroying {gameObject.name}. Another instance of object has been found");
+            Destroy(gameObject);
+        }
+        OnAwake();
+    }
+
+    public virtual void OnAwake()
+    {
+
+    }
+}
