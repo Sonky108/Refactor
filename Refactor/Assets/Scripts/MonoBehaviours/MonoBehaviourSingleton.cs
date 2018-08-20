abe7a74a41b79f059e44fcc80b24c2f639a18b44
Marcin Slowikowski <sonky108@gmail.com>
2018-08-20 18:39:57 +0200

    Mark MonoBehaviourSingleton as obsolete

--- Refactor/Assets/Scripts/MonoBehaviours/MonoBehaviourSingleton.cs
+++ Refactor/Assets/Scripts/MonoBehaviours/MonoBehaviourSingleton.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[Obsolete("Singleton is an anti-pattern")]
 public class MonoBehaviourSingleton<T> : MonoBehaviour  where T : class 
 {
     public static T instance;
